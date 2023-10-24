f98eb7ee7d0b28d75167b65e01f21ccc21216b44
“harshpatel” <harshkpatel310@gmail.com>
2023-10-24 00:55:47 -0400

    gsap

--- js/main.js
+++ js/main.js
@@ -1,37 +1,71 @@
 (() => {
-    // Function to handle model loading
+    // Data for hotspot content
+    const hotspotData = [
+      {
+        title: "Ear Tip",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
+      },
+      {
+        title: "Charging Points",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
+      },
+      {
+        title: "Battery",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
+      },
+      {
+        title: "Active Noise Cancellation",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
+      },
+      {
+        title: "Multi Function Button",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
+      },
+      {
+        title: "Speaker",
+        text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe, error.",
+      },
+    ];
+  
+    // Function to populate hotspot content
+    function populateHotspotContent() {
+      hotspotData.forEach((data, index) => {
+        const hotspotTitle = document.getElementById(`hotspot-title-${index + 1}`);
+        const hotspotText = document.getElementById(`hotspot-text-${index + 1}`);
+  
+        if (hotspotTitle && hotspotText) {
+          hotspotTitle.textContent = data.title;
+          hotspotText.textContent = data.text;
+        }
+      });
+    }
+  
+    const showInfo = function () {
+        const annotation = this.querySelector(".HotspotAnnotation");
+        gsap.to(annotation, { autoAlpha: 1  }); // Show with GSAP autoAlpha
+      };
+    
+      // Function to hide hotspot annotation using GSAP autoAlpha
+      const hideInfo = function () {
+        const annotation = this.querySelector(".HotspotAnnotation");
+        gsap.to(annotation, { autoAlpha: 0  }); // Hide with GSAP autoAlpha
+      };
+
+    // Add an event listener to load hotspot content when the page is ready
+    document.addEventListener("DOMContentLoaded", populateHotspotContent);
+  
     const modelLoaded = () => {
       // Select all hotspots
       const hotspots = document.querySelectorAll(".Hotspot");
   
-      // Hide hotspot annotations initially
-      hotspots.forEach(hotspot => {
-        const annotation = hotspot.querySelector(".HotspotAnnotation");
-        annotation.style.display = "none";
-      });
-  
-      // Show hotspot annotations on hover
-      hotspots.forEach(hotspot => {
-        hotspot.addEventListener("mouseover", showInfo);
-        hotspot.addEventListener("mouseout", hideInfo);
-      });
-    };
-  
-    // Function to show hotspot annotation on hover
-    const showInfo = function() {
-      // Show the annotation text on hover
-      const annotation = this.querySelector(".HotspotAnnotation");
-      annotation.style.display = "block";
-    };
-  
-    // Function to hide hotspot annotation when not hovered
-    const hideInfo = function() {
-      // Hide the annotation text when not hovered
-      const annotation = this.querySelector(".HotspotAnnotation");
-      annotation.style.display = "none";
+    hotspots.forEach((hotspot) => {
+      hotspot.addEventListener("mouseover", showInfo);
+      hotspot.addEventListener("mouseout", hideInfo);
+    });
     };
   
     // Add event listener for model load
     const model = document.querySelector("#model");
     model.addEventListener("load", modelLoaded);
-})(); 
+  })();
+  
